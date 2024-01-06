038685cba9a5d31574e14067ffd9ad18d4066030
Student <Student@Admins-MacBook-Air.local>
2024-01-06 11:42:55 -0500

    add render address

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://brad-travel.onrender.com/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
