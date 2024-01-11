1e075145ae9333d80fe3b9a84a64871905d85268
Student <Student@Admins-MacBook-Air.local>
2024-01-10 22:13:17 -0500

    fixed url for frontend

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://brad-travel.onrender.com";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://brad-travel-backend.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
